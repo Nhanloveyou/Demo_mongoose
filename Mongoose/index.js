9cc8f77db7ff2ca5222bbb8dd7ed6ab94665fc7b
Nhân Đặng <vannhanhk12@gmail.com>
2021-04-13 01:12:05 +0700

    2/3 demo

--- Mongoose/index.js
+++ Mongoose/index.js
@@ -1,11 +1,14 @@
 const mongoose = require('mongoose')
 const Courses = require('./models/courses')
 
+
+//Connect Mongoose
 const url = 'mongodb://localhost:27017/Mongoose_demo';
 const connect = mongoose.connect(url);
 
 connect.then((db) => {
     console.log('Connect Successfully');
+//Tạo một cơ sở dữ liệu
     var newCourse = Courses({
         name: 'Mongoosejs tutorial',
         description: 'Học xong bạn có thể làm việc với CSDL dễ dàng',
@@ -13,19 +16,22 @@ connect.then((db) => {
         level: 'easy',
         image: 'https://i.redd.it/2ff5574oso141.jpg'
     });
-
+// Lưu lại vào db
     newCourse.save()
-        .then((course) => {
-            console.log(course);
+        .then((nhan) => {
+            //In ra document vừa lưu
+            console.log(nhan);
 
             return Courses.find({}).exec();
         })
         .then((courses) => {
+            //in ra tất cả document trong tài liệu
             console.log(courses);
-
+            //Thực hiện xóa tất cả
             return Courses.deleteMany({});
         })
         .then(() => {
+            //Đóng kết nối đến DB
             return mongoose.connection.close();
         })
         .catch((err) => {
