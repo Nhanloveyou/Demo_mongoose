73c2b87b79ccc732f971cae4352a5390aa907ed5
Nhân Đặng <vannhanhk12@gmail.com>
2021-04-13 14:17:49 +0700

    Mongoose submit 2

--- Mongoose/index.js
+++ Mongoose/index.js
@@ -9,32 +9,60 @@ const connect = mongoose.connect(url);
 connect.then((db) => {
     console.log('Connect Successfully');
 //Tạo một cơ sở dữ liệu
-    var newCourse = Courses({
+    // var newCourse = Courses({
+    //     name: 'Mongoosejs tutorial',
+    //     description: 'Học xong bạn có thể làm việc với CSDL dễ dàng',
+    //     videoID: 'Adavdkajfls',
+    //     level: 'easy',
+    //     image: 'https://i.redd.it/2ff5574oso141.jpg'
+    // });
+
+    Courses.create({
         name: 'Mongoosejs tutorial',
         description: 'Học xong bạn có thể làm việc với CSDL dễ dàng',
         videoID: 'Adavdkajfls',
         level: 'easy',
         image: 'https://i.redd.it/2ff5574oso141.jpg'
-    });
-// Lưu lại vào db
-    newCourse.save()
-        .then((nhan) => {
-            //In ra document vừa lưu
-            console.log(nhan);
+    })
+    .then((nhan) => {
+        //In ra document vừa lưu
+        console.log(nhan);
 
-            return Courses.find({}).exec();
-        })
-        .then((courses) => {
-            //in ra tất cả document trong tài liệu
-            console.log(courses);
-            //Thực hiện xóa tất cả
-            return Courses.deleteMany({});
-        })
-        .then(() => {
-            //Đóng kết nối đến DB
-            return mongoose.connection.close();
-        })
-        .catch((err) => {
-            console.log(err);
-        })
-})
+        return Courses.find({}).exec();
+    })
+    .then((courses) => {
+        //in ra tất cả document trong tài liệu
+        console.log(courses);
+        //Thực hiện xóa tất cả
+        return Courses.deleteMany({});
+    })
+    .then(() => {
+        //Đóng kết nối đến DB
+        return mongoose.connection.close();
+    })
+    .catch((err) => {
+        console.log(err);
+    })
+});
+// Lưu lại vào db
+    // newCourse.save()
+//         .then((nhan) => {
+//             //In ra document vừa lưu
+//             console.log(nhan);
+
+//             return Courses.find({}).exec();
+//         })
+//         .then((courses) => {
+//             //in ra tất cả document trong tài liệu
+//             console.log(courses);
+//             //Thực hiện xóa tất cả
+//             return Courses.deleteMany({});
+//         })
+//         .then(() => {
+//             //Đóng kết nối đến DB
+//             return mongoose.connection.close();
+//         })
+//         .catch((err) => {
+//             console.log(err);
+//         })
+// })
